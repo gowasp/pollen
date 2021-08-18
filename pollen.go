8136b34be3bbc52fe75b126415274b1618a2f7bd
tnngo <tnngou@gmail.com>
2021-08-18 10:59:13 +0800

    feat: add '\n'

--- pollen.go
+++ pollen.go
@@ -80,8 +80,10 @@ func (p *Pollen) SubmitSubscribe() error {
 	buf := &bytes.Buffer{}
 	for _, v := range strs {
 		sln := append([]byte{byte(len(v))}, v...)
+		sln = append(sln, '\n')
 		buf.Write(sln)
 	}
+
 	if _, err := p.conn.Write(pkg.FIXED_SUBSCRIBE.Encode(buf.Bytes())); err != nil {
 		return err
 	}
